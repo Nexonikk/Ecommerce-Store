f73caa1a877a85f1f15dd04ed34cf1fdb4b142e5
Yusuf <143870776+Nexonikk@users.noreply.github.com>
2024-11-05 23:14:46 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://ecommerce-store-mu-coral.vercel.app/",
+    origin: "https://ecommerce-store-mu-coral.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
